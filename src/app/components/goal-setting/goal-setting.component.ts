1667b7cf03ca2489b7866de4394df8c20e978e21
GarryLed <s00256356@atu.ie>
2025-02-03 09:40:05 +0000

    Generating components for the view layer

---
+++ src/app/components/goal-setting/goal-setting.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-setting',
+  imports: [],
+  templateUrl: './goal-setting.component.html',
+  styleUrl: './goal-setting.component.scss'
+})
+export class GoalSettingComponent {
+
+}
---
+++ src/app/components/meal-log/meal-log.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meal-log',
+  imports: [],
+  templateUrl: './meal-log.component.html',
+  styleUrl: './meal-log.component.scss'
+})
+export class MealLogComponent {
+
+}
